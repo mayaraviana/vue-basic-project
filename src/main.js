93684db1dd219fc9243b59841a92c7dc05e8b6a4
Mayara Viana <54589016+mayaraviana@users.noreply.github.com>
2021-07-17 20:02:14 -0300

    Revert "add Bootstrap vue"
    This reverts commit d832f3484f61116edfbcd65d014e0b8dc2d4913a.

--- src/main.js
+++ src/main.js
@@ -1,13 +1,7 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import { BootstrapVue } from 'bootstrap-vue';
-
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
-
-Vue.use(BootstrapVue);
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
 Vue.config.productionTip = false;
 
@@ -15,4 +9,4 @@ new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app');
+}).$mount("#app");
